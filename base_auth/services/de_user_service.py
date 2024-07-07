a8e4af496dd438aa5c3d88daafca25da83b1a623
Agga <agga@Aggas-MacBook-Air.local>
2024-07-07 09:30:01 +0700

    [add] base_auth: optimnize or refactor

--- base_auth/services/de_user_service.py
+++ base_auth/services/de_user_service.py
@@ -13,20 +13,13 @@ def get_user(db: Session, user_id: int):
 
 def get_user_by_email(db: Session, email: str):
     return DeUser.get_by(db, email=email)
-    # return db.query(DeUserModel).filter(DeUserModel.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return DeUser.get_multi(db, skip=skip, limit=limit)
-    # return db.query(DeUserModel).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: DeUserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = DeUserModel(email=user.email, hashed_password=fake_hashed_password)
     return DeUser.create(db, db_user)
-
-    # db.add(db_user)
-    # db.commit()
-    # db.refresh(db_user)
-    # return db_user
