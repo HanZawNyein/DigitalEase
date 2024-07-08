4c6acc05315446fa4539ff9280e8841dfff17f0a
Agga <agga@Aggas-MacBook-Air.local>
2024-07-08 11:55:21 +0700

    [ADD] base_auth: sample route

--- base_auth/router/de_user_router.py
+++ base_auth/router/de_user_router.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -14,14 +16,35 @@ class DeUserRouter(CRUDRouter):
     update_schema = DeUserUpdate
     response_model = DeUser
 
-    # Example of overriding a method
-    async def create_object(self, obj_in: CreateSchemaType, db: Session = Depends(get_db)):
-        obj_in.password = self.hash_password(obj_in.password)  # Example of custom logic
-        return await super(DeUserRouter, self).create_object(obj_in, db)
+    def init_routes(self):
+        self.router.add_api_route("/register", self.register, response_model=self.response_model, methods=["POST"])
+        self.router.add_api_route("/login", self.read_objects, response_model=List[self.response_model],
+                                  methods=["POST"])
+        self.router.add_api_route("/forgot-password", self.forgot_password, response_model=self.response_model,
+                                  methods=["POST"])
+        self.router.add_api_route("/reset-password", self.reset_password, response_model=self.response_model,
+                                  methods=["POST"])
 
-    def hash_password(self, password: str) -> str:
-        # Implement password hashing here
-        return password + "hashed"
+    async def login(self, obj_in, db: Session = Depends(get_db)):
+        ...
+
+    async def register(self, obj_in: CreateSchemaType, db: Session = Depends(get_db)):
+        ...
+
+    async def forgot_password(self, obj_in, db: Session = Depends(get_db)):
+        ...
+
+    async def reset_password(self, obj_in, db: Session = Depends(get_db)):
+        ...
+
+    # Example of overriding a method
+    # async def create_object(self, obj_in: CreateSchemaType, db: Session = Depends(get_db)):
+    #     obj_in.password = self.hash_password(obj_in.password)  # Example of custom logic
+    #     return await super(DeUserRouter, self).create_object(obj_in, db)
+    #
+    # def hash_password(self, password: str) -> str:
+    #     # Implement password hashing here
+    #     return password + "hashed"
 
 
 de_user_router = DeUserRouter().router
