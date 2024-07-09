12af9754ff3901ca252364620e5efa9b54724089
Agga <agga@Aggas-MacBook-Air.local>
2024-07-09 16:56:18 +0700

    [add] base_todo: app m

--- base_todo/app.py
+++ base_todo/app.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI, Depends
 from base_auth.services.token import get_current_active_user
 from base_todo.router.de_todo_router import DeTodoRouter
 
-app = FastAPI(dependencies=[Depends(get_current_active_user)], title="Todo")
+app = FastAPI(dependencies=[Depends(get_current_active_user)], title="Todo",)
 
 app.include_router(DeTodoRouter().router)
